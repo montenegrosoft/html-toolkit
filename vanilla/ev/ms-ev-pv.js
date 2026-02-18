077bf137aacb02deab8f08b0de064a9339e47bac
Guilherme Montenegro <info@guimontenegro.dev>
2026-02-17 21:23:22 -0300

    Pageview event name fixing

--- vanilla/ev/ms-ev-pv.js
+++ vanilla/ev/ms-ev-pv.js
@@ -3,7 +3,7 @@
         if (typeof window.ms !== 'undefined' &&
             typeof window.ms.event === 'function') {
             setTimeout(() => {
-                ms.event({ facebookEvent: 'PageView', googleAnalyticsEvent: 'page_view' })
+                ms.event({ metaEvent: 'PageView', gaEvent: 'page_view' })
             }, 0)
         }
     })
